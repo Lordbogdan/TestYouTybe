340b08507230213ecf7d0417e8b2bf7c8e7d7972
b.marchenko <Dontapproachmei@mail.ru>
2020-04-13 16:58:01 +0400

    For

---
+++ src/test/java/TestTwo.java
@@ -0,0 +1,18 @@
+import org.w3c.dom.ls.LSOutput;
+
+import java.sql.SQLOutput;
+
+public class TestTwo {
+    public static void main (String[] args){
+        int i = 0;
+        while(true){
+            if (i==15){
+                break;
+            }
+            System.out.println(i);
+            i++;
+        }
+        System.out.println("Все, конец цикла");
+        System.out.println("пара-пара-пам");
+    }
+}
