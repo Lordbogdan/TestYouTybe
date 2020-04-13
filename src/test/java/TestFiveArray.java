56c6688fb7f96be4634cd75401ddcb7c37b1e58a
b.marchenko <Dontapproachmei@mail.ru>
2020-04-13 19:46:11 +0400

    Switch

---
+++ src/test/java/TestFiveArray.java
@@ -0,0 +1,11 @@
+public class TestFiveArray {
+    public static void main(String[] args) {
+        int[] numbers = new int[8];
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = i * 10;
+        }
+        for (int i = 0;i<numbers.length; i++){
+            System.out.println(numbers[i]);
+           }
+        }
+    }
